3972ad0da60b1fc4d206a4ebcafc4382b9fd585b
Amel Salibasic <nukecx@gmail.com>
2018-07-25 23:22:55 +0100

    base and update to 6.1.0

---
+++ src/Stellmart.App/ClientApp/src/app/core/core.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '../../../node_modules/@angular/router';
+import { CoreRoutingModule } from './core-routing.module';
+
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AuthenticationService } from './services/authentication/authentication.service';
+import { AuthGuardService } from './services/guard/auth-guard.service';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        CoreRoutingModule
+    ],
+    declarations: [
+        SignupComponent,
+        FooterComponent,
+        HeaderComponent
+    ],
+    exports: [
+        RouterModule,
+        HeaderComponent,
+        FooterComponent
+    ],
+    providers: [
+        AuthenticationService,
+        AuthGuardService
+    ]
+})
+export class CoreModule { }
