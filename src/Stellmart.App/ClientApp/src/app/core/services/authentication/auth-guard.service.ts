9083f1c78d905729c2b4da7776d07cc3a4914d25
Amel Salibasic <nukecx@gmail.com>
2018-08-06 23:16:56 +0100

    profile, passport, sidebar components base

--- src/Stellmart.App/ClientApp/src/app/core/services/authentication/auth-guard.service.ts
+++ src/Stellmart.App/ClientApp/src/app/core/services/authentication/auth-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+    constructor
+        (
+        private authenticationService: AuthenticationService
+        ) { }
+
+    public canActivate(): boolean {
+        if (this.authenticationService.isAuthorized()) {
+            return true;
+        }
+
+        return false;
+    }
 }
