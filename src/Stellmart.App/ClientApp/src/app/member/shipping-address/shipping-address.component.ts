d956e853ba02a1f4115280830436a9c6af568bf7
Amel Salibasic <nukecx@gmail.com>
2018-10-10 00:31:35 +0100

    save shipping address and value patching fix

--- src/Stellmart.App/ClientApp/src/app/member/shipping-address/shipping-address.component.ts
+++ src/Stellmart.App/ClientApp/src/app/member/shipping-address/shipping-address.component.ts
@@ -4,6 +4,7 @@ import { FormValidationService } from '../../shared/services/form-validation/for
 import { GooglePlacesResponse } from '../../shared/models/google-places-response.model';
 import { LocationResponse } from '../../shared/models/location/location-response.model';
 import { LocationService } from '../../shared/services/api/location/location.service';
+import { LocationRequest } from '../../shared/models/location/location-request.model';
 
 @Component({
   selector: 'app-shipping-address',
@@ -38,8 +39,8 @@ export class ShippingAddressComponent implements OnInit {
       locality: ['', Validators.required],
       administrative_area_level_1: ['', Validators.required],
       administrative_area_level_2: ['', Validators.required],
-      sublocality_level_1: ['', Validators.required],
-      neighborhood: ['', Validators.required],
+      sublocality_level_1: [''],
+      neighborhood: [''],
       postal_code: ['', Validators.required],
       country: ['', Validators.required]
     });
@@ -48,6 +49,18 @@ export class ShippingAddressComponent implements OnInit {
   public setAddress(addrObj: GooglePlacesResponse): void {
     this.zone.run(() => {
       this.googlePlacesResponse = addrObj;
+
+      this.form.patchValue({
+        route: this.googlePlacesResponse.route,
+        street_number: this.googlePlacesResponse.street_number,
+        locality: this.googlePlacesResponse.locality,
+        administrative_area_level_1: this.googlePlacesResponse.administrative_area_level_1,
+        administrative_area_level_2: this.googlePlacesResponse.administrative_area_level_2,
+        sublocality_level_1: this.googlePlacesResponse.sublocality_level_1,
+        neighborhood: this.googlePlacesResponse.neighborhood,
+        postal_code: this.googlePlacesResponse.postal_code,
+        country: this.googlePlacesResponse.country
+      });
     });
   }
 
@@ -56,5 +69,21 @@ export class ShippingAddressComponent implements OnInit {
       (addresses: LocationResponse[]) => {
         this.shippingAddresses = addresses;
       });
+  }
+
+  public save(): void {
+    if (this.form.valid) {
+
+      const request: LocationRequest = {
+        locationComponentsFromApp: JSON.stringify(this.form.value)
+      };
+
+      this.locationService.create(request)
+        .subscribe((result: LocationResponse) => {
+
+        });
+    } else {
+      this.formValidationService.validateAllFormFields(this.form);
+    }
   }
 }
