4303a044ff2e4e0d03a104eb0bc5b637a0784190
Amel Salibasic <nukecx@gmail.com>
2018-11-17 16:08:21 +0000

    account service update

--- src/Stellmart.App/ClientApp/src/app/shared/services/api/account/account.service.ts
+++ src/Stellmart.App/ClientApp/src/app/shared/services/api/account/account.service.ts
@@ -5,29 +5,44 @@ import { SignupRequest } from '../../../models/account/signup-request.model';
 import { SignupResponse } from '../../../models/account/signup-response.model';
 import { Observable } from 'rxjs';
 import { SecurityQuestionsResponse } from '../../../models/account/security-questions-response.model';
-import { ResetPasswordResponse } from 'src/app/shared/models/account/reset-password-response.model';
-import { ResetPasswordRequest } from 'src/app/shared/models/account/reset-password-request.model';
+import { ResetPasswordResponse } from '../../../models/account/reset-password-response.model';
+import { ResetPasswordRequest } from '../../../models/account/reset-password-request.model';
+import { ForgotPasswordRequest } from '../../../models/account/forgot-password-request.model';
+import { ForgotPasswordResponse } from '../../../models/account/forgot-password-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
   private baseUrl: string = APP_CONFIG.AppSettings.ApiUrl;
 
-  constructor(
-    private httpService: HttpClientService
-  ) { }
+  constructor(private httpService: HttpClientService) {}
 
   public signup(request: SignupRequest): Observable<SignupResponse> {
     return this.httpService.post(this.baseUrl + '/api/account/signup', request);
   }
 
   public getSecurityQuestions(): Observable<SecurityQuestionsResponse[]> {
-    return this.httpService.get(this.baseUrl + '/api/account/securityquestions');
+    return this.httpService.get(
+      this.baseUrl + '/api/account/securityquestions'
+    );
   }
 
-  public resetPassword(request: ResetPasswordRequest): Observable<ResetPasswordResponse> {
-    return this.httpService.post(this.baseUrl + '/api/account/resetpassword', request);
+  public resetPassword(
+    request: ResetPasswordRequest
+  ): Observable<ResetPasswordResponse> {
+    return this.httpService.post(
+      this.baseUrl + '/api/account/resetpassword',
+      request
+    );
+  }
+
+  public forgotPassword(
+    request: ForgotPasswordRequest
+  ): Observable<ForgotPasswordResponse> {
+    return this.httpService.post(
+      this.baseUrl + '/api/account/forgotpassword',
+      request
+    );
   }
 }
