2f28e326607ec5653737564581b2d7448af9b596
Amel Salibasic <nukecx@gmail.com>
2019-02-02 17:27:14 +0000

    ngrx store, effects, devtools set up

--- src/Stellmart.App/ClientApp/src/app/app.module.ts
+++ src/Stellmart.App/ClientApp/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
 
+import { EffectsModule } from '@ngrx/effects';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({} as ActionReducerMap<any>),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 100,
+      logOnly: false,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
