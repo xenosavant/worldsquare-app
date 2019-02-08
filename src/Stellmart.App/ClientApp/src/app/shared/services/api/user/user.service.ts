28649458ef2b7c14dcc3122c1da8f6444784a062
Amel Salibasic <nukecx@gmail.com>
2019-02-08 23:10:46 +0000

    added balance endpoint to user service

--- src/Stellmart.App/ClientApp/src/app/shared/services/api/user/user.service.ts
+++ src/Stellmart.App/ClientApp/src/app/shared/services/api/user/user.service.ts
@@ -3,19 +3,21 @@ import { UserResponse } from '../../../models/user/user-response.model';
 import { Observable } from 'rxjs';
 import { APP_CONFIG } from '../../config/config.service';
 import { HttpClientService } from '../../http/http-client.service';
+import { UserBalanceResponse } from '../../../models/user/user-balance-response.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UserService {
+  private baseUrl: string = APP_CONFIG.AppSettings.ApiUrl;
 
-    private baseUrl: string = APP_CONFIG.AppSettings.ApiUrl;
+  constructor(private httpService: HttpClientService) {}
 
-    constructor(
-        private httpService: HttpClientService
-    ) { }
+  public getCurrentLoggedUser(): Observable<UserResponse> {
+    return this.httpService.get(this.baseUrl + '/api/user/me');
+  }
 
-    public getCurrentLoggedUser(): Observable<UserResponse> {
-        return this.httpService.get(this.baseUrl + '/api/user/me');
-    }
+  public getBalance(): Observable<UserBalanceResponse> {
+    return this.httpService.get(this.baseUrl + '/api/user/balance');
+  }
 }
