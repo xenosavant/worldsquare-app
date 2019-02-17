d750d50d15df5e89f1ebfd0b496150629c6ef15e
Amel Salibasic <nukecx@gmail.com>
2019-02-17 01:29:14 +0000

    feat(load-config-redux): assign auth config values from state

--- src/Stellmart.App/ClientApp/src/app/shared/services/config/auth-config.service.ts
+++ src/Stellmart.App/ClientApp/src/app/shared/services/config/auth-config.service.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
 import { APP_CONFIG } from './config.service';
+import { AppFacade } from '../../../store/facades/app,facade';
+import { AppConfig } from '../../../app.config';
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 export class AuthConfigurationService {
-    public iss: string = APP_CONFIG.AppSettings.AuthUrl;
+  public iss: string;
 
-    public server: string = APP_CONFIG.AppSettings.AuthUrl;
+  public server: string;
 
-    public redirect_url: string = APP_CONFIG.AppSettings.AppUrl;
+  public redirect_url: string;
 
-    // This is required to get the signing keys so that the signiture of the Jwt can be validated.
-    public jwks_url: string = APP_CONFIG.AppSettings.AuthUrl + '/.well-known/openid-configuration/jwks';
+  // This is required to get the signing keys so that the signiture of the Jwt can be validated.
+  public jwks_url: string;
 
-    public client_id: string = 'implicitclient';
+  public client_id: string = 'implicitclient';
 
-    public response_type: string = 'id_token token';
+  public response_type: string = 'id_token token';
 
-    public scope: string = 'api1 openid profile';
+  public scope: string = 'api1 openid profile';
 
-    public post_logout_redirect_uri: string = APP_CONFIG.AppSettings.AppUrl;
+  public post_logout_redirect_uri: string;
+
+  constructor(private appFacade: AppFacade) {
+    appFacade.getConfig.subscribe((config: AppConfig) => {
+      this.iss = config.AppSettings.AuthUrl;
+      this.server = config.AppSettings.AuthUrl;
+      this.redirect_url = config.AppSettings.AppUrl;
+      this.jwks_url =
+        config.AppSettings.AuthUrl + '/.well-known/openid-configuration/jwks';
+      this.post_logout_redirect_uri = config.AppSettings.AppUrl;
+    });
+  }
 }
